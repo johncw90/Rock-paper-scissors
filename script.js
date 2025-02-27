07707ce8cbdd3765941f854f1d0de6ab275b5ecc
johncw90 <johncw90@yahoo.com>
2025-02-26 19:16:27 -0600

    Add winner tracking

--- script.js
+++ script.js
@@ -34,19 +34,32 @@ let playGame = () => {
       roundMsg = "It's a tie!";
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
       roundMsg = `You win! ${humanChoice} beats ${computerChoice}`;
+      humanScore = humanScore + 1;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
       roundMsg = `You win! ${humanChoice} beats ${computerChoice}`;
+      humanScore = humanScore + 1;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
       roundMsg = `You win! ${humanChoice} beats ${computerChoice}`;
+      humanScore = humanScore + 1;
     } else {
       roundMsg = `You lose! ${computerChoice} beats ${humanChoice}`;
+      computerScore = computerScore + 1;
     }
     console.log(roundMsg);
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
   };
 
   for (let i = 1; i <= 5; i++) {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
+    if (i === 5 && humanScore > computerScore) {
+      console.log("You win the game!");
+    } else if (i === 5 && computerScore > humanScore) {
+      console.log("Computer won the game!");
+    } else if (i === 5 && computerScore === humanScore) {
+      console.log("No winner!");
+    }
   }
 };
